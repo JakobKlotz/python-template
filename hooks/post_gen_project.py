8894148adcd568178883a9ef0fb6820daa891019
JakobKlotz <jakob.klotz@mci.edu>
2025-03-27 08:51:03 +0100

    feat: make Docker optional #2

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,13 @@
 import subprocess
+from pathlib import Path
 
 subprocess.run(["git", "init"])
 subprocess.run(["pre-commit", "autoupdate"])
 subprocess.run(["pre-commit", "install", "--install-hooks"])
 subprocess.run(["uv", "sync"])
+
+# remove all Docker files
+if "{{ cookiecutter.use_docker }}" == "n":
+    Path("docker-compose.yaml").unlink()
+    Path("Dockerfile").unlink()
+    Path(".dockerignore").unlink()
