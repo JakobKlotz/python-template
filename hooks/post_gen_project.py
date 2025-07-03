c98cf785f8c5eb2c59c6dad002e7669914f078a3
JakobKlotz <jakob.klotz@mci.edu>
2025-07-03 15:02:33 +0200

    feat: provide options for python version, use of pre-commit and Docker

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,12 +2,18 @@ import subprocess
 from pathlib import Path
 
 subprocess.run(["git", "init"])
-subprocess.run(["pre-commit", "autoupdate"])
-subprocess.run(["pre-commit", "install", "--install-hooks"])
+
+if "{{ cookiecutter.use_precommit }}" == "yes":
+    subprocess.run(["pre-commit", "autoupdate"])
+    subprocess.run(["pre-commit", "install", "--install-hooks"])
+
+else:
+    Path(".pre-commit-config.yaml").unlink()
+
 subprocess.run(["uv", "sync"])
 
 # remove all Docker files
-if "{{ cookiecutter.use_docker }}" == "n":
+if "{{ cookiecutter.use_docker }}" == "no":
     Path("docker-compose.yaml").unlink()
     Path("Dockerfile").unlink()
     Path(".dockerignore").unlink()
